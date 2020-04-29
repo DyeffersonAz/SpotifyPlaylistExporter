f0039e61f2cac267f7c2e3484db701b5ecd2f7dd
DyeffersonAz <dyeffersonazevedo@hotmail.com>
2020-04-29 15:14:11 -0300

    Setting some things up

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from tokens import *
+from download import *
+import sys
+import spotipy as spotify
+
+""" print(str(sys.argv))
+try:
+    if sys.argv:
+        username = sys.argv[1]
+except IndexError:
+    print("Use username as an argument!")
+    sys.exit()
+
+
+user_token = spotify.util.prompt_for_user_token(username, scope=app_scope, client_id=app_client_id, client_secret=app_client_secret, redirect_uri=app_redirect_uri, show_dialog=True) """
+
 
