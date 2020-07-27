f0113e5f6e4a984833c0df8e827a3eecd81b01a1
Dyefferson Azevedo <dyeffersonazevedo@hotmail.com>
2020-07-26 21:20:18 -0300

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk() # Creates the window
+
+welcome = Label(root, text="This is the Spotify Extractor")
+welcome.pack()
+
+root.mainloop() # Shows the window, actually
