0bc894c41bc37b9bfe3953e7b998c948ff55a6dd
Dyefferson Azevedo <dyeffersonazevedo@hotmail.com>
2020-08-08 14:49:52 -0300

    Basic GUI functionality

--- gui.py
+++ gui.py
@@ -1,8 +1,36 @@
 from tkinter import *
+import tkinter.messagebox
+from pathlib import Path
+from os import name as osname
+from main import getSongs
+
+def btnGetPressed():
+    tkinter.messagebox.showinfo(message="You may be asked for putting the redirect url on terminal, put it there and the software will start!")
+    getSongs(txtUserID.get())
 
 root = Tk() # Creates the window
 
-welcome = Label(root, text="This is the Spotify Extractor")
-welcome.pack()
+root.title("Spotify Extractor")
+root.resizable(False, False)
+root.iconphoto(True, PhotoImage(file=str(Path("images/icon128.png"))))
+
+lblWelcome = Label(root, text="Spotify Extractor", font=("sans-serif", 28, "normal"))
+lblWelcome.grid(row=0, columnspan=4)
+
+lblTypeUser = Label(root, text="Type in your user ID for Spotify:", font=("sans-serif", 12, "normal"))
+lblTypeUser.grid(row=1, column=0)
+
+txtUserID = Entry(root)
+txtUserID.grid(row=1, column=1)
+
+btnGet = Button(root, text="Obter músicas", command=btnGetPressed)
+btnGet.grid(row=2, columnspan=2)
+
+lblCopyright = Label(root, text="Copyright Dyefferson AzevedoⒸ 2020")
+lblCopyright.grid(row=3, columnspan=4)
+
+if osname == "nt": # Making icon work with windows
+    import ctypes
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('mycompany.myproduct.subproduct.version')
 
 root.mainloop() # Shows the window, actually
--- main.py
+++ main.py
@@ -6,13 +6,24 @@ import tracks_parser
 import downloader
 import configparser
 
-print("Vamos começar a baixar suas favoritas, mas primeiro vamos preparar algumas coisas...")
-cnfg = configparser.ConfigParser()
+def getSongs(userID):
+    spotifier.auth(userID)
+    savedTracks = spotifier.get_saved_tracks()
+    downloader.download_list(tracks_parser.parse(json.dumps(savedTracks)))
 
-cnfg.read("config.cfg")
 
-spotifier.auth(cnfg.get("SPOTIFY", "USERNAME")) #Authing with Spotify
 
-savedTracks = spotifier.get_saved_tracks() #Getting a list of dictionaries from Spotify
-print("Pronto, agora vamos começar a baixar!")
-downloader.download_list(tracks_parser.parse(json.dumps(savedTracks))) # Downloads from a list of songs dumped in a json string and parsed by tracks-parser.py
+def main():
+    print("Vamos começar a baixar suas favoritas, mas primeiro vamos preparar algumas coisas...")
+    cnfg = configparser.ConfigParser()
+
+    cnfg.read("config.cfg")
+
+    spotifier.auth(cnfg.get("SPOTIFY", "USERNAME")) #Authing with Spotify
+
+    savedTracks = spotifier.get_saved_tracks() #Getting a list of dictionaries from Spotify
+    print("Pronto, agora vamos começar a baixar!")
+    downloader.download_list(tracks_parser.parse(json.dumps(savedTracks))) # Downloads from a list of songs dumped in a json string and parsed by tracks-parser.py
+
+if __name__ == "__main__":
+    main()
